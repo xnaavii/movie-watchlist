a6c971691a6a3a031d747bab02d5b66213324860
Ivan Petrović <137888945+xnaavii@users.noreply.github.com>
2026-06-29 21:07:21 +0100

    Connect drizzle with neon and better auth

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,11 @@
-import { drizzle } from 'drizzle-orm/node-postgres'
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from "./schema.ts";
 
-import * as schema from './schema.ts'
+if (!process.env.DATABASE_URL) {
+	throw new Error("DATABASE_URL environment variable is missing");
+}
 
-export const db = drizzle(process.env.DATABASE_URL!, { schema })
+const sql = neon(process.env.DATABASE_URL);
+
+export const db = drizzle({ client: sql, schema });
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,14 @@
-import { betterAuth } from 'better-auth'
-import { tanstackStartCookies } from 'better-auth/tanstack-start'
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { tanstackStartCookies } from "better-auth/tanstack-start";
+import { db } from "@/db";
 
 export const auth = betterAuth({
-  emailAndPassword: {
-    enabled: true,
-  },
+	emailAndPassword: {
+		enabled: true,
+	},
+	database: drizzleAdapter(db, {
+    provider: "pg",
+	}),
   plugins: [tanstackStartCookies()],
-})
+});
