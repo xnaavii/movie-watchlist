292f93c2d2f2b6119d6503e63254aacc931c0341
Ivan Petrović <137888945+xnaavii@users.noreply.github.com>
2026-06-30 19:22:34 +0100

    Create features folder and tmdb specific utils folder

--- src/features/tmdb/utils/tmdb.functions.ts
+++ src/features/tmdb/utils/tmdb.functions.ts
@@ -2,7 +2,7 @@ import { createServerFn } from "@tanstack/react-start";
 import { getMovieById } from "./tmdb.server";
 
 export const getMovie = createServerFn({ method: "GET" })
-	.validator((data: { id: number }) => data)
-	.handler(async ({ data }) => {
-		return getMovieById(data.id);
-	});
+  .validator((data: { id: number }) => data)
+  .handler(async ({ data }) => {
+    return getMovieById(data.id);
+  });
