70294c8f7a752b79cbf809a501534394873dcddd
Ivan Petrović <137888945+xnaavii@users.noreply.github.com>
2026-07-09 19:37:04 +0100

    Remove error and isPending from use suspense on explore page

--- src/routes/explore.tsx
+++ src/routes/explore.tsx
@@ -1,7 +1,6 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 import { FeaturedMovies } from "#/features/movies/components/FeaturedMovies";
-import { FeaturedMoviesSkeleton } from "#/features/movies/components/FeaturedMoviesSkeleton";
 import { MoviesSection } from "#/features/movies/components/MoviesSection";
 import { movieQueries } from "#/features/movies/queries";
 
@@ -13,19 +12,11 @@ export const Route = createFileRoute("/explore")({
 });
 
 function ExplorePage() {
-	const { data, isPending, isError, error } = useSuspenseQuery(
-		movieQueries.list("now_playing"),
-	);
+	const { data } = useSuspenseQuery(movieQueries.list("now_playing"));
 
 	return (
 		<div className="flex flex-col gap-8">
-			{isPending ? (
-				<FeaturedMoviesSkeleton />
-			) : isError ? (
-				<p>{error.message}</p>
-			) : (
-				<FeaturedMovies movies={data.results} />
-			)}
+			<FeaturedMovies movies={data.results} />
 			<div className="flex flex-col gap-20 p-6">
 				<MoviesSection title="Popular" list="popular" />
 				<MoviesSection title="In Theaters" list="now_playing" />
