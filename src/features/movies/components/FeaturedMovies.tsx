b1fdf4685a6530a466abe290e1493be9762a1847
Ivan Petrović <137888945+xnaavii@users.noreply.github.com>
2026-07-11 18:17:34 +0100

    Adjust typography on the featured movies component

--- src/features/movies/components/FeaturedMovies.tsx
+++ src/features/movies/components/FeaturedMovies.tsx
@@ -39,16 +39,16 @@ export function FeaturedMovies({ movies }: { movies: MovieResultItem[] }) {
 				<p>No Image</p>
 			)}
 			<div className="absolute inset-0 bg-linear-to-t from-black to-transparent z-1" />
-			<div className="absolute inset-0 max-w-2xl p-6 flex flex-col gap-4 justify-end z-9">
-				<h1 className="text-4xl font-semibold drop-shadow-lg">{movie.title}</h1>
-				<p className="text-secondary">{movie.overview}</p>
+			<div className="absolute inset-0 p-6 flex flex-col gap-6 max-w-5xl justify-end z-9">
+				<h1 className="text-5xl font-semibold min-w-0">{movie.title}</h1>
+				<p className="text-muted-foreground text-lg">{movie.overview}</p>
 				<div className="flex gap-2">
-					<Button>
+					<Button size="lg">
 						<BookmarkPlus />
 						Add to watchlist
 					</Button>
 
-					<Button variant="secondary" asChild>
+					<Button variant="secondary" asChild size="lg">
 						<Link to="/movies/$id" params={{ id: `${movie.id}` }}>
 							Details
 						</Link>
