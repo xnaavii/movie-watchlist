81bef69369f702ddea31ad27999e0e362ab3c363
Ivan Petrović <137888945+xnaavii@users.noreply.github.com>
2026-07-13 11:28:23 +0100

    Add a log out button on the profile page

--- src/routes/_protected/profile.tsx
+++ src/routes/_protected/profile.tsx
@@ -1,5 +1,8 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
+import { createFileRoute, redirect, useRouter } from "@tanstack/react-router";
+import { LogOut } from "lucide-react";
+import { Button } from "#/components/ui/button";
 import { getSession } from "#/lib/auth.functions";
+import { authClient } from "#/lib/auth-client";
 
 export const Route = createFileRoute("/_protected/profile")({
 	beforeLoad: async () => {
@@ -16,6 +19,22 @@ export const Route = createFileRoute("/_protected/profile")({
 
 function ProfilePage() {
 	const { user } = Route.useRouteContext();
+	const router = useRouter();
 
-	return <div>Logged in as, {user.name}!</div>;
+	const logout = async () => {
+		await authClient.signOut();
+		await router.invalidate();
+	};
+
+	return (
+		<div className="mt-10 p-2 md:p-6 md:mt-0">
+			<div className="flex items-center justify-between">
+				<p>Logged in as, {user.name}!</p>
+				<Button variant="destructive" onClick={logout}>
+					Log out
+					<LogOut />
+				</Button>
+			</div>
+		</div>
+	);
 }
