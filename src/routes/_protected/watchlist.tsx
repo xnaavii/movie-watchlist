93a1c90c23b8bbf7d028f3ce7a62e0d6b355e42e
Ivan Petrović <137888945+xnaavii@users.noreply.github.com>
2026-07-14 22:32:13 +0100

    Remove unused import

--- src/routes/_protected/watchlist.tsx
+++ src/routes/_protected/watchlist.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute, Link } from "@tanstack/react-router";
+import { createFileRoute } from "@tanstack/react-router";
 import { MovieCard } from "#/features/movies/components/MovieCard";
 import { getUserWatchlist } from "#/features/watchlist/server/watchlist.functions";
 
