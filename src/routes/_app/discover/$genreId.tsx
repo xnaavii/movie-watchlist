d5361234effe2352482f4f85b94962eb432c21ce
Ivan Petrović <137888945+xnaavii@users.noreply.github.com>
2026-07-28 12:22:47 +0100

    Display genre name and featured movies on genre discover genre page

--- src/routes/_app/discover/$genreId.tsx
+++ src/routes/_app/discover/$genreId.tsx
@@ -1,4 +1,7 @@
+import { useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
+import { FeaturedMoviesCarousel } from "#/features/movies/components/FeaturedMoviesCarousel";
+import { movieQueries } from "#/features/movies/queries";
 
 export const Route = createFileRoute("/_app/discover/$genreId")({
 	params: {
@@ -8,11 +11,33 @@ export const Route = createFileRoute("/_app/discover/$genreId")({
 			return { genreId: Number(genreId) };
 		},
 	},
+	loader: async ({ context, params }) => {
+		const [genres, movies] = await Promise.all([
+			context.queryClient.ensureQueryData(movieQueries.genres({})),
+			context.queryClient.ensureQueryData(
+				movieQueries.discover({ with_genres: params.genreId }),
+			),
+		]);
+
+		return { genres, movies };
+	},
 	component: DiscoverGenrePage,
 });
 
 function DiscoverGenrePage() {
 	const { genreId } = Route.useParams();
+	const { data: genres } = useSuspenseQuery(movieQueries.genres({}));
+	const { data: movies } = useSuspenseQuery(
+		movieQueries.discover({ with_genres: genreId }),
+	);
+	const selectedGenre = genres.genres.find((genre) => genre.id === genreId);
 
-	return <div>Hello "/_app/discover/{genreId}</div>;
+	return (
+		<>
+			<h1 className="text-2xl lg:text-3xl tracking-tighter">
+				Discover {selectedGenre?.name}
+			</h1>
+			<FeaturedMoviesCarousel movies={movies.results} />
+		</>
+	);
 }
