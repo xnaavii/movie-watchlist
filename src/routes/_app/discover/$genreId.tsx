c61d396581862180a6b2e3c49ee6ea745f7f37ea
Ivan Petrović <137888945+xnaavii@users.noreply.github.com>
2026-08-04 09:09:05 +0100

    Fix error where unauthorized user tries to access the genreid page

--- src/routes/_app/discover/$genreId.tsx
+++ src/routes/_app/discover/$genreId.tsx
@@ -1,4 +1,4 @@
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { useQuery, useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute, Link } from "@tanstack/react-router";
 import { BackButton } from "#/components/BackButton";
 import { Button } from "#/components/ui/button";
@@ -7,6 +7,7 @@ import { MovieRow } from "#/features/movies/components/MovieRow";
 import { movieQueries } from "#/features/movies/queries";
 import { normalizeMovie } from "#/features/movies/utils";
 import { watchlistQueries } from "#/features/watchlist/queries";
+import { getSession } from "#/lib/auth.functions";
 
 export const Route = createFileRoute("/_app/discover/$genreId")({
 	params: {
@@ -17,35 +18,54 @@ export const Route = createFileRoute("/_app/discover/$genreId")({
 		},
 	},
 	loader: async ({ context, params }) => {
-		const [genres, movies] = await Promise.all([
-			context.queryClient.ensureQueryData(movieQueries.genres({})),
-			context.queryClient.ensureQueryData(
-				movieQueries.discover({ with_genres: params.genreId }),
-			),
-			context.queryClient.ensureQueryData(
-				watchlistQueries.moviesByGenreId(params.genreId),
-			),
-			context.queryClient.ensureQueryData(watchlistQueries.watchlistStatuses()),
-		]);
+		const session = await getSession();
 
-		return { genres, movies };
+		const [genres, movies, watchlistMovies, watchlistStatuses] =
+			await Promise.all([
+				context.queryClient.ensureQueryData(movieQueries.genres({})),
+				context.queryClient.ensureQueryData(
+					movieQueries.discover({ with_genres: params.genreId }),
+				),
+				session
+					? context.queryClient.ensureQueryData(
+							watchlistQueries.moviesByGenreId(params.genreId),
+						)
+					: null,
+				session
+					? context.queryClient.ensureQueryData(
+							watchlistQueries.watchlistStatuses(),
+						)
+					: null,
+			]);
+
+		return {
+			genres,
+			movies,
+			watchlistMovies,
+			watchlistStatuses,
+			isAuthenticated: !!session,
+		};
 	},
 	component: DiscoverGenrePage,
 });
 
 function DiscoverGenrePage() {
+	const { isAuthenticated } = Route.useLoaderData();
 	const { genreId } = Route.useParams();
 	const { data: genres } = useSuspenseQuery(movieQueries.genres({}));
 	const { data: movies } = useSuspenseQuery(
 		movieQueries.discover({ with_genres: genreId }),
 	);
-	const { data: watchlistStatuses } = useSuspenseQuery(
-		watchlistQueries.watchlistStatuses(),
-	);
-	const { data: watchlistMovies } = useSuspenseQuery(
-		watchlistQueries.moviesByGenreId(genreId),
-	);
+	const { data: watchlistStatuses } = useQuery({
+		...watchlistQueries.watchlistStatuses(),
+		enabled: isAuthenticated,
+	});
+	const { data: watchlistMovies } = useQuery({
+		...watchlistQueries.moviesByGenreId(genreId),
+		enabled: isAuthenticated,
+	});
 	const selectedGenre = genres.genres.find((genre) => genre.id === genreId);
+	console.log(isAuthenticated);
 
 	return (
 		<>
@@ -59,21 +79,26 @@ function DiscoverGenrePage() {
 			<MovieBackdropMarquee
 				movies={movies.results.map((movie) => normalizeMovie(movie))}
 			/>
-			<section className="flex flex-col gap-4">
-				<div className="flex justify-between">
-					<h2 className="text-xl lg:text-2xl tracking-tighter">
-						{selectedGenre?.name} Movies in Your Watchlist
-					</h2>
-					<Button variant="link" asChild>
-						<Link to="/watchlist">See all</Link>
-					</Button>
-				</div>
 
-				<MovieRow
-					movies={watchlistMovies}
-					watchlistStatuses={watchlistStatuses}
-				/>
-			</section>
+			{isAuthenticated && (
+				<section className="flex flex-col gap-4">
+					<div className="flex justify-between">
+						<h2 className="text-xl lg:text-2xl tracking-tighter">
+							{selectedGenre?.name} Movies in Your Watchlist
+						</h2>
+						<Button variant="link" asChild>
+							<Link to="/watchlist">See all</Link>
+						</Button>
+					</div>
+
+					{watchlistMovies && watchlistStatuses ? (
+						<MovieRow
+							movies={watchlistMovies}
+							watchlistStatuses={watchlistStatuses}
+						/>
+					) : null}
+				</section>
+			)}
 		</>
 	);
 }
